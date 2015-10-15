b759a85e39fe3770707551aba5b2e9d4301134de
Kaoru Kohashigawa <dev@kaoruk.com>
2015-10-14 21:03:52 -0700

    Get prompter specs to green by doing it right by rspec

--- spec/utils/prompter_spec.rb
+++ spec/utils/prompter_spec.rb
@@ -9,9 +9,8 @@ module Utils
       end
 
       it "will add custom text" do
-        stub_prompter(:output)
+        expect(Prompter).to receive(:output).with("hibby jibby:> ")
         Prompter.run(custom_text: "hibby jibby")
-        expect(Prompter).to receive(:output).with("hibby jibby :>")
       end
 
       context "when a player is passed" do
@@ -19,9 +18,8 @@ module Utils
 
         it "will display a player's stats" do
           player_prompt_match = match(Regexp.new("#{player.hp}.+HP.+#{player.mp}.+MP"))
-          readline_dub = class_double("Readline").as_stubbed_const
 
-          expect(readline_dub).to receive(:readline).with(player_prompt_match)
+          expect(Prompter).to receive(:output).with(player_prompt_match)
           Prompter.run(player: player)
         end
       end
